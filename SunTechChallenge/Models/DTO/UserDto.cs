c12180cb959849bedcb88d582d1572bc3b763bcf
Mark Olaes <markolaes08@gmail.com>
2023-07-21 21:14:12 +0800

    Added final changes cosmosDB connection, event grid, cosmosdb trigger

---
+++ SunTechChallenge/Models/DTO/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunTechChallenge.Models.DTO
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double BirthdayInEpoch { get; set; }
+        public string Email { get; set; }
+    }
+}
